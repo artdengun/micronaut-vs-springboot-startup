172203644a24d3703dab350743a155922b85ab5f
Razvan Bunea <razvan.bunea@orange.com>
2019-03-04 22:43:01 +0100

    Real Lunchtime timer

--- hello-mn/src/main/java/hello/mn/Application.java
+++ hello-mn/src/main/java/hello/mn/Application.java
@@ -1,10 +1,17 @@
 package hello.mn;
 
+import io.micronaut.core.version.VersionUtils;
 import io.micronaut.runtime.Micronaut;
+
+import java.time.LocalDateTime;
 
 public class Application {
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        System.out.println(":: Micronaut :: ("+VersionUtils.MICRONAUT_VERSION+") - " + LocalDateTime.now());
         Micronaut.run(Application.class);
+        long duration = System.currentTimeMillis() - startTime;
+        System.out.println(":: Micronaut :: DONE - " + LocalDateTime.now() + " - duration: " + duration + " ms");
     }
 }
 
--- hello-sb/src/main/java/hello/sb/Application.java
+++ hello-sb/src/main/java/hello/sb/Application.java
@@ -2,12 +2,16 @@ package hello.sb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
+		long startTime = System.currentTimeMillis();
+		System.out.println(":: Spring Boot :: - " + LocalDateTime.now());
 		SpringApplication.run(Application.class, args);
+		long duration = System.currentTimeMillis() - startTime;
+		System.out.println(":: Spring Boot :: DONE - " + LocalDateTime.now() + " - duration: " + duration + " ms");
 	}
 }
